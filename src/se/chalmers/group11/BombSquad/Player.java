40f778a5b443262f4cad2fca4b5a4e7fa94a9e39
Marcus Tyren <marcus.tyren@gmail.com>
2012-03-22 17:22:07 +0100

    Modified in Player.java

--- src/se/chalmers/group11/BombSquad/Player.java
+++ src/se/chalmers/group11/BombSquad/Player.java
@@ -1,5 +1,38 @@
 package se.chalmers.group11.BombSquad;
 
 public class Player {
-
+	
+	int x;
+	int y;
+	
+	public void movePlayerDir(int direction){
+		
+		int deltaX;
+		int deltaY;
+		
+		if(direction==1){
+			deltaX =1;
+			deltaY=0;
+		}
+		if(direction==2){
+			deltaX =-1;
+			deltaY=0;
+		}
+		if(direction==3){
+			deltaX =0;
+			deltaY=1;
+		}
+		if(direction==4){
+			deltaX =0;
+			deltaY=-1;
+		}
+		
+		GameTile t = GameBoard.getTile(x+deltaX, y+deltaY);
+				t.recievesPlayer(this);
+	}
+	public void movePlayer(int x, int y){
+		newX=x;
+		newY=y;
+		//player.getTile(newX,newY);
+	}
 }
