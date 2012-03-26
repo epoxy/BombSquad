4a89bae99bf97eeedde715c337591fbd68b0d130
Marcus Tyren <marcus.tyren@gmail.com>
2012-03-26 15:04:47 +0200

    meetings folder added

--- src/se/chalmers/group11/BombSquad/GamePlayState.java
+++ src/se/chalmers/group11/BombSquad/GamePlayState.java
@@ -7,6 +7,8 @@ import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 import org.newdawn.slick.tiled.TiledMap;
 
+import se.chalmers.group11.BombSquad.GameTile;
+
 public class GamePlayState extends BasicGameState {
 	int stateID = -1;
 	//private TiledMap map;
@@ -17,21 +19,21 @@ public class GamePlayState extends BasicGameState {
 	}
 
 	@Override
-	public void init(GameContainer arg0, StateBasedGame arg1)
+	public void init(GameContainer gc, StateBasedGame sbg)
 			throws SlickException {
 		//map = new TiledMap("lib/TileBoard.tmx");
 
 	}
 
 	@Override
-	public void render(GameContainer arg0, StateBasedGame arg1, Graphics arg2)
+	public void render(GameContainer gc, StateBasedGame sbg, Graphics g)
 			throws SlickException {
 		//map.render(0, 0);
 
 	}
 
 	@Override
-	public void update(GameContainer arg0, StateBasedGame arg1, int arg2)
+	public void update(GameContainer gc, StateBasedGame sbg, int i)
 			throws SlickException {
 		// TODO Auto-generated method stub
 
