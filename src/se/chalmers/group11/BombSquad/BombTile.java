7e509b209aa31c8355427962185a03d187e49ccc
Anton Palmqvist <palmqvist.anton@gmail.com>
2012-03-28 10:57:24 +0200

    BombTile-class added

---
+++ src/se/chalmers/group11/BombSquad/BombTile.java
@@ -0,0 +1,38 @@
+package se.chalmers.group11.BombSquad;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.Timer;
+
+public class BombTile implements GameTile{
+	public BombTile(){
+		System.out.println("Bomb läggs ut");
+		int delay = 3000; //milliseconds
+		  ActionListener taskPerformer = new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				System.out.println("Eld ritas ut");
+				//fire
+			}
+		  };
+		  new Timer(delay, taskPerformer).start();
+	}
+	@Override
+	public boolean recievesPlayer() {
+		return false;
+	}
+
+	@Override
+	public void receivesFire() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void performOnPlayer() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
