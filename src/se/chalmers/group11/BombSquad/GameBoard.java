e65f4adff710d785ad63645a4cde45a7ceec5e43
Anton Palmqvist <palmqvist.anton@gmail.com>
2012-05-02 12:18:52 +0200

    flyttar instansvariavler

--- src/se/chalmers/group11/BombSquad/GameBoard.java
+++ src/se/chalmers/group11/BombSquad/GameBoard.java
@@ -6,13 +6,12 @@ import java.awt.event.ActionListener;
 import javax.swing.Timer;
 
 public class GameBoard {
-
+	private int sideLength = 11;
+	private GameTile gameTiles[][];// ändra till private!
+	private GameTile gameTilestmp[][];
 
 
 	private GameBoard() {
-		player = new Player[2];
-		player[0] = new Player(0, 0);
-		player[1] = new Player(10, 10);
 		gameTilestmp = new GameTile[sideLength][sideLength];
 		gameTiles = new GameTile[sideLength][sideLength];
 		for (int i = 0; i < gameTiles.length; i++) {
