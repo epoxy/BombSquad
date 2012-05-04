ae4d4ccfbcea24298c12f017a56236acec5ddb6d
Anton Palmqvist <palmqvist.anton@gmail.com>
2012-05-04 12:34:24 +0200

    Added testExplodeBomb

--- test/se/chalmers/group11/BombSquad/TestGame.java
+++ test/se/chalmers/group11/BombSquad/TestGame.java
@@ -7,10 +7,13 @@ import org.junit.Test;
 public class TestGame {
 
 	@Test
-	public void testGetBoard() {
+	public void testExplodeBomb() {
 		Game game = Game.getInstance();
-		game.getBoard().getTile(game.getPlayer(1).getX(),
-		game.getPlayer(1).getY()).performOnPlayer(1);
+		game.explodeBomb(2, 2, 0);
+		game.getBoard().setToTile(2, 7, new EmptyTile());//Sets to EmptyTile since it otherwise is random
+		GameTile gT = game.getBoard().getTile(2, 3);
+		GameTile gT2 = game.getBoard().getTile(2, 7);
+		assertTrue(gT instanceof FireTile && gT2 instanceof EmptyTile); // The logical check
 	}
 
 }
