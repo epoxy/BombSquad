75bceb06ef28f4920b1e481f1fe5ed88473aa482
Tomas <tomsel1@hotmail.com>
2012-05-07 23:51:16 +0200

    new Images for players so you can now choose between six different players

--- src/se/chalmers/group11/BombSquadGUI/ChooseCharacterState.java
+++ src/se/chalmers/group11/BombSquadGUI/ChooseCharacterState.java
@@ -15,12 +15,19 @@ public class ChooseCharacterState extends BasicGameState {
 	private int nextButtonX = 500;
 	private int nextButtonY = 500;
 	private int thumbsY = 120;
+	private int thumbsY2 = 200;
 	private int p1DevilX = 50;
 	private int p2DevilX = 360;
 	private int p1BombManX = 120;
 	private int p2BombManX = 430;
 	private int p1GingerX = 190;
 	private int p2GingerX = 500;
+	private int p1KingX = 50;
+	private int p2kingX = 360;
+	private int p1manX = 120;
+	private int p2manX = 430;
+	private int p1mantwoX = 190;
+	private int p2mantwoX = 500;
 	private float nextButtonScale = 0.5f;
 	private float nextButtonScaleStep = 0.0001f;
 	private float p1DevilScale = 1.5f;
@@ -29,6 +36,12 @@ public class ChooseCharacterState extends BasicGameState {
 	private float p2BombManScale = 1.5f;
 	private float p1GingerScale = 1.5f;
 	private float p2GingerScale = 1.5f;
+	private float p1KingScale = 1.5f;
+	private float p2KingScale = 1.5f;
+	private float p1ManScale = 1.5f;
+	private float p2ManScale = 1.5f;
+	private float p1ManTwoScale = 1.5f;
+	private float p2ManTwoScale = 1.5f;
 	private float thumbsScaleStep = 0.0005f;
 	private Image nextButton;
 	private Image playerOneText;
@@ -36,21 +49,25 @@ public class ChooseCharacterState extends BasicGameState {
 	private Image devil;
 	private Image bombMan;
 	private Image ginger;
+	private Image king;
+	private Image man;
+	private Image manTwo;
 	private static String playerOneSkin = "Devil";
 	private static String playerTwoSkin = "BombMan";
+
 	public ChooseCharacterState(int stateID) {
 		this.stateID = stateID;
 
 	}
 
-	public static String getPlayerOneSkin(){
+	public static String getPlayerOneSkin() {
 		return playerOneSkin;
-		
-	}
-	public static String getPlayerTwoSkin(){
-		return playerTwoSkin;
+
 	}
 
+	public static String getPlayerTwoSkin() {
+		return playerTwoSkin;
+	}
 
 	@Override
 	public void init(GameContainer container, StateBasedGame game)
@@ -61,6 +78,9 @@ public class ChooseCharacterState extends BasicGameState {
 		devil = new Image("Images/devilDOWN.gif");
 		bombMan = new Image("Images/bombManDOWN.gif");
 		ginger = new Image("Images/gingerDOWN.gif");
+		king = new Image("Images/kingDOWN.gif");
+		man = new Image("Images/manDOWN.gif");
+		manTwo = new Image("Images/mantwoDOWN.gif");
 
 	}
 
@@ -76,7 +96,12 @@ public class ChooseCharacterState extends BasicGameState {
 		devil.draw(p2DevilX, thumbsY, p2DevilScale);
 		bombMan.draw(p2BombManX, thumbsY, p2BombManScale);
 		ginger.draw(p2GingerX, thumbsY, p2GingerScale);
-
+		king.draw(p2kingX, thumbsY2, p2KingScale);
+		king.draw(p1KingX, thumbsY2, p1KingScale);
+		man.draw(p1manX, thumbsY2, p1ManScale);
+		man.draw(p2manX, thumbsY2, p2ManScale);
+		manTwo.draw(p1mantwoX, thumbsY2, p1ManTwoScale);
+		manTwo.draw(p2mantwoX, thumbsY2, p2ManTwoScale);
 
 	}
 
@@ -95,46 +120,91 @@ public class ChooseCharacterState extends BasicGameState {
 		boolean insideP2BombMan = false;
 		boolean insideP1Ginger = false;
 		boolean insideP2Ginger = false;
+		boolean insideP1king = false;
+		boolean insideP2king = false;
+		boolean insideP1man = false;
+		boolean insideP2man = false;
+		boolean insideP1manTwo = false;
+		boolean insideP2manTwo = false;
 
-
-		if (mouseX >= nextButtonX && mouseX <= nextButtonX + nextButton.getWidth()*nextButtonScale
+		if (mouseX >= nextButtonX
+				&& mouseX <= nextButtonX + nextButton.getWidth()
+						* nextButtonScale
 				&& mouseY >= nextButtonY
-				&& mouseY <= nextButtonY + nextButton.getHeight()*nextButtonScale) {
+				&& mouseY <= nextButtonY + nextButton.getHeight()
+						* nextButtonScale) {
 			insideNextButton = true;
 		}
-		if (mouseX >= p1DevilX && mouseX <= p1DevilX + devil.getWidth()*p1DevilScale
+		if (mouseX >= p1DevilX
+				&& mouseX <= p1DevilX + devil.getWidth() * p1DevilScale
 				&& mouseY >= thumbsY
-				&& mouseY <= thumbsY + devil.getHeight()*p1DevilScale) {
+				&& mouseY <= thumbsY + devil.getHeight() * p1DevilScale) {
 			insideP1Devil = true;
 		}
-		if (mouseX >= p2DevilX && mouseX <= p2DevilX + devil.getWidth()*p2DevilScale
+		if (mouseX >= p2DevilX
+				&& mouseX <= p2DevilX + devil.getWidth() * p2DevilScale
 				&& mouseY >= thumbsY
-				&& mouseY <= thumbsY + devil.getHeight()*p2DevilScale) {
+				&& mouseY <= thumbsY + devil.getHeight() * p2DevilScale) {
 			insideP2Devil = true;
 		}
-		if (mouseX >= p1BombManX && mouseX <= p1BombManX + bombMan.getWidth()*p1BombManScale
+		if (mouseX >= p1BombManX
+				&& mouseX <= p1BombManX + bombMan.getWidth() * p1BombManScale
 				&& mouseY >= thumbsY
-				&& mouseY <= thumbsY + bombMan.getHeight()*p1BombManScale) {
+				&& mouseY <= thumbsY + bombMan.getHeight() * p1BombManScale) {
 			insideP1BombMan = true;
 		}
-		if (mouseX >= p2BombManX && mouseX <= p2BombManX + bombMan.getWidth()*p2BombManScale
+		if (mouseX >= p2BombManX
+				&& mouseX <= p2BombManX + bombMan.getWidth() * p2BombManScale
 				&& mouseY >= thumbsY
-				&& mouseY <= thumbsY + bombMan.getHeight()*p2BombManScale) {
+				&& mouseY <= thumbsY + bombMan.getHeight() * p2BombManScale) {
 			insideP2BombMan = true;
 		}
-		if (mouseX >= p1GingerX && mouseX <= p1GingerX + ginger.getWidth()*p1GingerScale
+		if (mouseX >= p1GingerX
+				&& mouseX <= p1GingerX + ginger.getWidth() * p1GingerScale
 				&& mouseY >= thumbsY
-				&& mouseY <= thumbsY + ginger.getHeight()*p1GingerScale) {
+				&& mouseY <= thumbsY + ginger.getHeight() * p1GingerScale) {
 			insideP1Ginger = true;
 		}
-		if (mouseX >= p2GingerX && mouseX <= p2GingerX + ginger.getWidth()*p2GingerScale
+		if (mouseX >= p2GingerX
+				&& mouseX <= p2GingerX + ginger.getWidth() * p2GingerScale
 				&& mouseY >= thumbsY
-				&& mouseY <= thumbsY + ginger.getHeight()*p2GingerScale) {
+				&& mouseY <= thumbsY + ginger.getHeight() * p2GingerScale) {
 			insideP2Ginger = true;
 		}
-
-
-
+		if (mouseX >= p1KingX
+				&& mouseX <= p1KingX + king.getWidth() * p1KingScale
+				&& mouseY >= thumbsY2
+				&& mouseY <= thumbsY2 + king.getHeight() * p1KingScale) {
+			insideP1king = true;
+		}
+		if (mouseX >= p2kingX
+				&& mouseX <= p2kingX + king.getWidth() * p2KingScale
+				&& mouseY >= thumbsY2
+				&& mouseY <= thumbsY2 + king.getHeight() * p2KingScale) {
+			insideP2king = true;
+		}
+		if (mouseX >= p1manX && mouseX <= p1manX + man.getWidth() * p1ManScale
+				&& mouseY >= thumbsY2
+				&& mouseY <= thumbsY2 + man.getHeight() * p1ManScale) {
+			insideP1man = true;
+		}
+		if (mouseX >= p2manX && mouseX <= p2manX + man.getWidth() * p2ManScale
+				&& mouseY >= thumbsY2
+				&& mouseY <= thumbsY2 + man.getHeight() * p2ManScale) {
+			insideP2man = true;
+		}
+		if (mouseX >= p1mantwoX
+				&& mouseX <= p1mantwoX + man.getWidth() * p1ManScale
+				&& mouseY >= thumbsY2
+				&& mouseY <= thumbsY2 + man.getHeight() * p1ManScale) {
+			insideP1manTwo = true;
+		}
+		if (mouseX >= p2mantwoX
+				&& mouseX <= p2mantwoX + man.getWidth() * p2ManScale
+				&& mouseY >= thumbsY2
+				&& mouseY <= thumbsY2 + man.getHeight() * p2ManScale) {
+			insideP2manTwo = true;
+		}
 		if (insideNextButton) {
 			if (nextButtonScale < 0.55f)
 				nextButtonScale += nextButtonScaleStep * delta;
@@ -153,7 +223,7 @@ public class ChooseCharacterState extends BasicGameState {
 			if (p1DevilScale < 1.65f)
 				p1DevilScale += thumbsScaleStep * delta;
 			if (input.isMouseButtonDown(Input.MOUSE_LEFT_BUTTON)) {
-				playerOneSkin = "Devil";		
+				playerOneSkin = "Devil";
 			}
 		} else {
 			if (p1DevilScale > 1.5f) {
@@ -164,8 +234,8 @@ public class ChooseCharacterState extends BasicGameState {
 			if (p1BombManScale < 1.65f)
 				p1BombManScale += thumbsScaleStep * delta;
 			if (input.isMouseButtonDown(Input.MOUSE_LEFT_BUTTON)) {
-				playerOneSkin = "BombMan";	
-				
+				playerOneSkin = "BombMan";
+
 			}
 		} else {
 
@@ -177,7 +247,7 @@ public class ChooseCharacterState extends BasicGameState {
 			if (p1GingerScale < 1.65f)
 				p1GingerScale += thumbsScaleStep * delta;
 			if (input.isMouseButtonDown(Input.MOUSE_LEFT_BUTTON)) {
-				playerOneSkin = "Ginger";	
+				playerOneSkin = "Ginger";
 				System.out.println("p1 ginger");
 			}
 		} else {
@@ -189,7 +259,7 @@ public class ChooseCharacterState extends BasicGameState {
 			if (p2DevilScale < 1.65f)
 				p2DevilScale += thumbsScaleStep * delta;
 			if (input.isMouseButtonDown(Input.MOUSE_LEFT_BUTTON)) {
-				playerTwoSkin = "Devil";		
+				playerTwoSkin = "Devil";
 			}
 		} else {
 			if (p2DevilScale > 1.5f) {
@@ -200,7 +270,7 @@ public class ChooseCharacterState extends BasicGameState {
 			if (p2BombManScale < 1.65f)
 				p2BombManScale += thumbsScaleStep * delta;
 			if (input.isMouseButtonDown(Input.MOUSE_LEFT_BUTTON)) {
-				playerTwoSkin = "BombMan";			
+				playerTwoSkin = "BombMan";
 			}
 		} else {
 
@@ -212,7 +282,7 @@ public class ChooseCharacterState extends BasicGameState {
 			if (p2GingerScale < 1.65f)
 				p2GingerScale += thumbsScaleStep * delta;
 			if (input.isMouseButtonDown(Input.MOUSE_LEFT_BUTTON)) {
-				playerTwoSkin = "Ginger";			
+				playerTwoSkin = "Ginger";
 				System.out.println("p2 ginger");
 			}
 		} else {
@@ -220,10 +290,73 @@ public class ChooseCharacterState extends BasicGameState {
 				p2GingerScale -= thumbsScaleStep * delta;
 			}
 		}
-
+		if (insideP1king) {
+			if (p1KingScale < 1.65f)
+				p1KingScale += thumbsScaleStep * delta;
+			if (input.isMouseButtonDown(Input.MOUSE_LEFT_BUTTON)) {
+				playerOneSkin = "king";
+			}
+		} else {
+			if (p1KingScale > 1.5f) {
+				p1KingScale -= thumbsScaleStep * delta;
+			}
+		}
+		if (insideP2king) {
+			if (p2KingScale < 1.65f)
+				p2KingScale += thumbsScaleStep * delta;
+			if (input.isMouseButtonDown(Input.MOUSE_LEFT_BUTTON)) {
+				playerTwoSkin = "king";
+			}
+		} else {
+			if (p2KingScale > 1.5f) {
+				p2KingScale -= thumbsScaleStep * delta;
+			}
+		}
+		if (insideP1man) {
+			if (p1ManScale < 1.65f)
+				p1ManScale += thumbsScaleStep * delta;
+			if (input.isMouseButtonDown(Input.MOUSE_LEFT_BUTTON)) {
+				playerOneSkin = "man";
+			}
+		} else {
+			if (p1ManScale > 1.5f) {
+				p1ManScale -= thumbsScaleStep * delta;
+			}
+		}
+		if (insideP2man) {
+			if (p2ManScale < 1.65f)
+				p2ManScale += thumbsScaleStep * delta;
+			if (input.isMouseButtonDown(Input.MOUSE_LEFT_BUTTON)) {
+				playerTwoSkin = "man";
+			}
+		} else {
+			if (p2ManScale > 1.5f) {
+				p2ManScale -= thumbsScaleStep * delta;
+			}
+		}
+		if (insideP1manTwo) {
+			if (p1ManTwoScale < 1.65f)
+				p1ManTwoScale += thumbsScaleStep * delta;
+			if (input.isMouseButtonDown(Input.MOUSE_LEFT_BUTTON)) {
+				playerOneSkin = "manTwo";
+			}
+		} else {
+			if (p1ManTwoScale > 1.5f) {
+				p1ManTwoScale -= thumbsScaleStep * delta;
+			}
+		}
+		if (insideP2manTwo) {
+			if (p2ManTwoScale < 1.65f)
+				p2ManTwoScale += thumbsScaleStep * delta;
+			if (input.isMouseButtonDown(Input.MOUSE_LEFT_BUTTON)) {
+				playerTwoSkin = "manTwo";
+			}
+		} else {
+			if (p2ManTwoScale > 1.5f) {
+				p2ManTwoScale -= thumbsScaleStep * delta;
+			}
+		}
 	}
-	
-
 
 	@Override
 	public int getID() {
--- src/se/chalmers/group11/utils/SpriteSheets.java
+++ src/se/chalmers/group11/utils/SpriteSheets.java
@@ -6,7 +6,6 @@ import org.newdawn.slick.SlickException;
 
 public class SpriteSheets {
 
-
 	private Animation sprite, up, down, left, right;
 
 	public SpriteSheets(String s) throws SlickException {
@@ -15,35 +14,65 @@ public class SpriteSheets {
 		Image[] movementLeft = new Image[2];
 		Image[] movementRight = new Image[2];
 
-		if(s=="BombMan"){
-			movementUp[0] =  new Image("Images/bombManUP.gif");
+		if (s == "BombMan") {
+			movementUp[0] = new Image("Images/bombManUP.gif");
 			movementUp[1] = new Image("Images/bombManUP2.gif");
-			movementDown[0] =  new Image("Images/bombManDOWN.gif");
+			movementDown[0] = new Image("Images/bombManDOWN.gif");
 			movementDown[1] = new Image("Images/bombManDOWN2.gif");
-			movementLeft[0] =  new Image("Images/bombManLEFT.gif");
+			movementLeft[0] = new Image("Images/bombManLEFT.gif");
 			movementLeft[1] = new Image("Images/bombManLEFT2.gif");
-			movementRight[0] =  new Image("Images/bombManRIGHT.gif");
+			movementRight[0] = new Image("Images/bombManRIGHT.gif");
 			movementRight[1] = new Image("Images/bombManRIGHT2.gif");
 		}
-		if(s=="Devil"){
-			movementUp[0] =  new Image("Images/devilUP.gif");
+		if (s == "Devil") {
+			movementUp[0] = new Image("Images/devilUP.gif");
 			movementUp[1] = new Image("Images/devilUP2.gif");
-			movementDown[0] =  new Image("Images/devilDOWN.gif");
+			movementDown[0] = new Image("Images/devilDOWN.gif");
 			movementDown[1] = new Image("Images/devilDOWN2.gif");
-			movementLeft[0] =  new Image("Images/devilLEFT.gif");
+			movementLeft[0] = new Image("Images/devilLEFT.gif");
 			movementLeft[1] = new Image("Images/devilLEFT2.gif");
-			movementRight[0] =  new Image("Images/devilRIGHT.gif");
+			movementRight[0] = new Image("Images/devilRIGHT.gif");
 			movementRight[1] = new Image("Images/devilRIGHT2.gif");
 		}
-		if(s=="Ginger"){
-			movementUp[0] =  new Image("Images/gingerUP.gif");
+		if (s == "Ginger") {
+			movementUp[0] = new Image("Images/gingerUP.gif");
 			movementUp[1] = new Image("Images/gingerUP2.gif");
-			movementDown[0] =  new Image("Images/gingerDOWN.gif");
+			movementDown[0] = new Image("Images/gingerDOWN.gif");
 			movementDown[1] = new Image("Images/gingerDOWN2.gif");
-			movementLeft[0] =  new Image("Images/gingerLEFT.gif");
+			movementLeft[0] = new Image("Images/gingerLEFT.gif");
 			movementLeft[1] = new Image("Images/gingerLEFT2.gif");
-			movementRight[0] =  new Image("Images/gingerRIGHT.gif");
+			movementRight[0] = new Image("Images/gingerRIGHT.gif");
 			movementRight[1] = new Image("Images/gingerRIGHT2.gif");
+		}
+		if (s == "king") {
+			movementUp[0] = new Image("Images/kingUP.gif");
+			movementUp[1] = new Image("Images/kingUP2.gif");
+			movementDown[0] = new Image("Images/kingDOWN.gif");
+			movementDown[1] = new Image("Images/kingDOWN2.gif");
+			movementLeft[0] = new Image("Images/kingLEFT.gif");
+			movementLeft[1] = new Image("Images/kingLEFT2.gif");
+			movementRight[0] = new Image("Images/kingRIGHT.gif");
+			movementRight[1] = new Image("Images/kingRIGHT2.gif");
+		}
+		if (s == "man") {
+			movementUp[0] = new Image("Images/manUP.gif");
+			movementUp[1] = new Image("Images/manUP2.gif");
+			movementDown[0] = new Image("Images/manDOWN.gif");
+			movementDown[1] = new Image("Images/manDOWN2.gif");
+			movementLeft[0] = new Image("Images/manLEFT.gif");
+			movementLeft[1] = new Image("Images/manLEFT2.gif");
+			movementRight[0] = new Image("Images/manRIGHT.gif");
+			movementRight[1] = new Image("Images/manRIGHT2.gif");
+		}
+		if (s == "manTwo") {
+			movementUp[0] = new Image("Images/mantwoUP.gif");
+			movementUp[1] = new Image("Images/mantwoUP2.gif");
+			movementDown[0] = new Image("Images/mantwoDOWN.gif");
+			movementDown[1] = new Image("Images/mantwoDOWN2.gif");
+			movementLeft[0] = new Image("Images/mantwoLEFT.gif");
+			movementLeft[1] = new Image("Images/mantwoLEFT2.gif");
+			movementRight[0] = new Image("Images/mantwoRIGHT.gif");
+			movementRight[1] = new Image("Images/mantwoRIGHT2.gif");
 		}
 
 		int[] duration = { 150, 150 };
@@ -53,7 +82,6 @@ public class SpriteSheets {
 		right = new Animation(movementRight, duration, false);
 		sprite = right;
 	}
-
 
 	public void AnimationUp() {
 		sprite = up;
