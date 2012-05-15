ec737df8804137a3173228f1c96640f3484998b2
Anton Palmqvist <palmqvist.anton@gmail.com>
2012-05-15 23:54:49 +0200

    continued javadocing

--- src/se/chalmers/group11/core/Enemy.java
+++ src/se/chalmers/group11/core/Enemy.java
@@ -1,21 +1,62 @@
+/*
+ * Enemy
+ * 
+ * Version 1.0
+ *
+ * Date 2012-05-15
+ * 
+ * No rights reserved
+ */
+
+
 package se.chalmers.group11.core;
 
+/**
+ *  
+        
+Class representing the enemy in the game. Knows the position 
+of the enemy and can move it to new positions.
+ *
+ * @version      
+        
+1.0 15 May 2012
+ * @author          
+        
+Anton Palmqvist
+ */	
 public class Enemy {
+	
+	/*Varibles containing the instance's position. Also sets 
+	 * the starting position to coordinates x=6 and y=6.*/
 	private int x=6;
 	private int y=6;
-
+	
+	/**
+	 * Getter for the enemys x-coordinate.
+	 * 
+	 * @return the x-position of the enemy
+	 */
 	public int getX() {
 		return x;
 	}
-
+	
+	/**
+	 * Getter for the enemys y-coordinate.
+	 * 
+	 * @return the y-position of the enemy
+	 */
 	public int getY() {
 		return y;
 	}
+	
+	/**
+	 * Moves the enemy to the new position according to the parameters.
+	 * 
+	 * @param deltaX the amount of steps the enemy will go x-wise
+	 * @param deltaY the amount of steps the enemy will go y-wise
+	 */
 	public void move(int deltaX, int deltaY) {
 		x+=deltaX;
 		y+=deltaY;
-	}
-	public void killPlayer(){
-		
 	}
 }
--- src/se/chalmers/group11/core/LoserKeeper.java
+++ src/se/chalmers/group11/core/LoserKeeper.java
@@ -1,3 +1,14 @@
+/*
+ * LoserKeeper
+ * 
+ * Version 1.0
+ *
+ * Date 2012-05-15
+ * 
+ * No rights reserved
+ */
+
+
 package se.chalmers.group11.core;
 
 import org.newdawn.slick.state.StateBasedGame;
@@ -7,9 +18,22 @@ import se.chalmers.group11.eventbus.EventBus;
 import se.chalmers.group11.eventbus.IEventHandler;
 import se.chalmers.group11.main.Main;
 
+/**
+ *  
+        
+A class made to transefer the game to the GameOver-state, 
+showing the results of who won the game.
+ *
+ * @version      
+        
+1.0 18 May 2012
+ * @author          
+        
+Anton Palmqvist
+ */
+
 public class LoserKeeper implements IEventHandler{
 	private StateBasedGame sb;
-	private Integer loser;
 	
 	public LoserKeeper(StateBasedGame sb){
 		EventBus.INSTANCE.register(this);
@@ -20,7 +44,6 @@ public class LoserKeeper implements IEventHandler{
 	public void onEvent(Event evt) {
 		if(evt.getTag()==Event.Tag.PLAYER_KILLED){
 			System.out.println("Got event" + evt.getValue());
-			loser=(Integer)evt.getValue();
 			sb.enterState(Main.GAMEOVERSTATE);
 		}
 	}
