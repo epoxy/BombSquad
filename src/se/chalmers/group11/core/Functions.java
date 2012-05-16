a48c7935e61af9c4d6d1e3b1a1d62dbf7c4b5eee
Anton Palmqvist <palmqvist.anton@gmail.com>
2012-05-16 19:01:26 +0200

    fixed the land on poweritembug

--- src/se/chalmers/group11/core/Functions.java
+++ src/se/chalmers/group11/core/Functions.java
@@ -36,15 +36,24 @@ public class Functions {
 		iB.setToTile(0, 0, TileFactory.getEmptyTile());
 		iB.setToTile(1, 0, TileFactory.getEmptyTile());
 		iB.setToTile(0, 1, TileFactory.getEmptyTile());
+		/*The three tmpTiles in the upper left corner are also being set to
+		 * EmptyTiles. This is done in case any of the tmpTiles were set to a 
+		 * box before and therefore, if not turned to an EmptyTile, could be 
+		 * turned into at powerup when blown up*/
+		iB.setTmpToTile(0, 0, TileFactory.getEmptyTile());
+		iB.setTmpToTile(1, 0, TileFactory.getEmptyTile());
+		iB.setTmpToTile(0, 1, TileFactory.getEmptyTile());
 		
 		/*A randomisation determines which should be the last EmptyTile 
 		 * in the L-shape.*/
 		double rand = Math.random();
 		if(rand<0.5){//Randomizing the last emptyTile
 			iB.setToTile(2, 0, TileFactory.getEmptyTile());
+			iB.setTmpToTile(2, 0, TileFactory.getEmptyTile());
 		}
 		else{
 			iB.setToTile(0, 2, TileFactory.getEmptyTile());
+			iB.setTmpToTile(0, 2, TileFactory.getEmptyTile());
 		}
 		
 		/*The three tiles in the lower right corner are being set to
@@ -53,14 +62,24 @@ public class Functions {
 		iB.setToTile(10, 9, TileFactory.getEmptyTile());
 		iB.setToTile(9, 10, TileFactory.getEmptyTile());
 		
+		/*The three tmpTiles in the lower right corner are also being set to
+		 * EmptyTiles. This is done in case any of the tmpTiles were set to a 
+		 * box before and therefore, if not turned to an EmptyTile, could be 
+		 * turned into at powerup when blown up*/
+		iB.setTmpToTile(10, 10, TileFactory.getEmptyTile());
+		iB.setTmpToTile(10, 9, TileFactory.getEmptyTile());
+		iB.setTmpToTile(9, 10, TileFactory.getEmptyTile());
+		
 		/*A randomisation determines which should be the last EmptyTile 
 		 * in the L-shape.*/
 		rand = Math.random();
 		if(rand<0.5){//Randomizing the last emptyTile
 			iB.setToTile(8, 10, TileFactory.getEmptyTile());
+			iB.setTmpToTile(8, 10, TileFactory.getEmptyTile());
 		}
 		else{
 			iB.setToTile(10, 8, TileFactory.getEmptyTile());
+			iB.setTmpToTile(10, 8, TileFactory.getEmptyTile());
 		}
 
 	}
