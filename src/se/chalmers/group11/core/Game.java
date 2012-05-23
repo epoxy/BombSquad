b154f6309d3d176618e86310841a30dcf2a384f8
Henrik Andersson <heenrik@hotmail.com>
2012-05-24 01:53:18 +0200

    added timer to gameoverhandler

--- src/se/chalmers/group11/core/Game.java
+++ src/se/chalmers/group11/core/Game.java
@@ -137,7 +137,7 @@ public class Game implements IEventHandler {
 	 *            variable representing the player who placed the bomb
 	 * @bombCountdown starts bomb timer
 	 */
-	public void bombCountdown(final int bombX, final int bombY,
+	private void bombCountdown(final int bombX, final int bombY,
 			final int playerNumber) {
 		ActionListener taskPerformer = new ActionListener() {
 			@Override
@@ -164,7 +164,7 @@ public class Game implements IEventHandler {
 	 * @author TomasSelldén 
 	 * @author HenrikAndersson made the method call to placeFire work recursively to get rid of multiple loops.
 	 */
-	public void explodeBomb(int bombX, int bombY, int playerNumber) {
+	private void explodeBomb(int bombX, int bombY, int playerNumber) {
 
 		int firePower = player[playerNumber-1].getFirePower();
 		explodeDirection(bombX, bombY, Direction.CENTER, firePower);
@@ -180,6 +180,7 @@ public class Game implements IEventHandler {
 
 	/**
 	 * Handles weather or not to go on with the explosion process in the set direction.
+
 	 * @param bombX
 	 *            x coordinate of placed bomb
 	 * @param bombY
--- src/se/chalmers/group11/core/GameOverHandler.java
+++ src/se/chalmers/group11/core/GameOverHandler.java
@@ -11,6 +11,11 @@
 
 package se.chalmers.group11.core;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.Timer;
+
 import org.newdawn.slick.state.StateBasedGame;
 
 import se.chalmers.group11.eventbus.Event;
@@ -20,21 +25,21 @@ import se.chalmers.group11.main.Main;
 
 /**
  *  
-        
+
 A class made to transefer the game to the GameOver-state, 
 showing the results of who won the game.
  *
  * @version      
-        
+
 1.0 18 May 2012
  * @author          
-        
+
 Anton Palmqvist
  */
 
 public class GameOverHandler implements IEventHandler{
 	private StateBasedGame sbg;
-	
+
 	/**
 	 * Constructor for creating a new LoserKeeper.
 	 * 
@@ -48,7 +53,19 @@ public class GameOverHandler implements IEventHandler{
 	@Override
 	public void onEvent(Event evt) {
 		if(evt.getTag()==Event.Tag.PLAYER_KILLED){
-			sbg.enterState(Main.GAMEOVERSTATE); //Transfers to the GameOverState
+
+			ActionListener taskPerformer = new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent arg0) {
+					sbg.enterState(Main.GAMEOVERSTATE);
+
+				}
+			};
+
+			Timer t = new Timer(1000, taskPerformer);
+
+			t.setRepeats(false);
+			t.start();
 		}
 	}
 }
